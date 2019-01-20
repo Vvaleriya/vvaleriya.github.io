45631ca79866ab576e65e27572401985f2b2a9f8
Valeriya <46245657+Vvaleriya@users.noreply.github.com>
2019-01-20 13:25:06 +0200

    Add files via upload

--- js/jquery.js
+++ js/jquery.js
@@ -31,7 +31,7 @@ $('.myButton1').click(function() {
     $('.myButton').animate({ 'width': '300px', 'left':'25%', 'height':'100px','font-size':'38px', 'padding-top':'50px'},800, function(){document.location="game.html";});
 }); 
     //третья кнопка
-    $('.myButton3').click(function() {
+    $('#btn3').click(function() {
   $('.myButton1').animate({opacity: '0', 'margin-left': '1500px'},2000)  ;
     $('.myButton').animate({opacity: '0', 'margin-left': '1500px'},2000)  ;
     $('.myButton3').animate({ 'margin-top': '600px'},2000)  ;
@@ -44,6 +44,22 @@ $('.myButton1').click(function() {
      $('.myButton3').css({ 'position': 'absolute','left':'200px','top':'150px'},2000) ;
     $('.divv').animate({opacity: 0}, 300);
     $('.myButton3').animate({ 'width': '300px', 'left':'25%', 'height':'100px','font-size':'38px', 'padding-top':'50px'},800, function(){document.location="media.html";});
+}); 
+    
+    //четвертая кнопка
+    $('#btn4').click(function() {
+  $('.myButton1').animate({opacity: '0', 'margin-left': '1500px'},2000)  ;
+    $('.myButton').animate({opacity: '0', 'margin-left': '1500px'},2000)  ;
+    $('.myButton3').animate({ 'margin-top': '600px'},2000)  ;
+    $('header').animate({ 'top': '200px'},2000)  ;
+    //2 etap ya sha umru
+    $('.divv').animate({ 'height': '600px', 'width':'800px','left':'88px' },2000);
+    $('.divv').css({'background': 'url(images/done.gif) no-repeat center', 'background-size': '800px'});
+    //3tiy 
+    $('.myButton3').animate({ 'margin-left': '200px', 'margin-top':'100px'},100)  ;
+     $('.myButton3').css({ 'position': 'absolute','left':'200px','top':'150px'},2000) ;
+    $('.divv').animate({opacity: 0}, 300);
+    $('.myButton3').animate({ 'width': '300px', 'left':'25%', 'height':'100px','font-size':'38px', 'padding-top':'50px'},800, function(){document.location="adapt.html";});
 }); 
     
 });
